9f89d2671c08ecf65cbcc134696acf669abe5d2d
Renan Oliveira <rdpereira2010@hotmail.com>
2024-09-09 22:23:50 -0300

    💄 setup light and dark mode

---
+++ src/lib/components/action/theme.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    import Icon from "../icon.svelte";
+
+    let theme = $state<string | null>(null)
+
+    onMount(() => {
+        theme = localStorage.getItem('theme');
+
+        if (theme == null)
+            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    });
+</script>
+
+<button 
+    type="button" 
+    class="btn btn-lg btn-primary mask mask-squircle fixed top-2 right-2 *:text-2xl" 
+    data-toggle-theme="dark,light" 
+    data-act-class="ACTIVECLASS"
+>
+    {#if theme === 'light'}
+        <Icon icon="solar:sun-2-broken" />
+    {:else}
+        <Icon icon="solar:moon-broken" />
+    {/if}
+</button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,28 @@
 import type { Config } from 'tailwindcss';
 
+import daisyui from 'daisyui';
+
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {}
 	},
-
-	plugins: []
+	plugins: [daisyui],
+	daisyui: {
+		themes: [
+			{
+				light: {
+					...require("daisyui/src/theming/themes")["light"],
+					"base-100": "#f2f2f2",
+					"base-200": "#fafafa",
+					"base-300": "#ffffff"
+				},
+				dark: {
+					...require("daisyui/src/theming/themes")["dark"],
+					"base-100": "#000000"
+				}
+			}
+		]
+	}
 } as Config;
